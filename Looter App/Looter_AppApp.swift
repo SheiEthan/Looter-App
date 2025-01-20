dba7fdcd71aece116985a03adee3c6d9c6623f41
sheiethan <ethan.tillier@etu.univ-smb.fr>
2025-01-20 09:05:28 +0100

    Initial Commit

---
+++ Looter App/Looter_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Looter_AppApp.swift
+//  Looter App
+//
+//  Created by Ethan TILLIER on 1/20/25.
+//
+
+import SwiftUI
+
+@main
+struct Looter_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
